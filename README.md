c7372dc03c502d515e8acc9a77a85b562720511b
vallenov <a2lenov@mail.ru>
2021-12-24 21:46:36 +0300

    Добавлен парсинг аргументов командной строки -u (username) и -p (path). Добавлено описание

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # all-repo-loader
 Clones all repositories from your github account
+
+Usage:
+cd all-repo-loader
+python3.* load.py -u gitusername -p where/do/you/want/to/put/repos
+
+Only public repositories are cloned
+
+It is advisable to log into GitHub in advance
--- loader.py
+++ loader.py
@@ -2,15 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import configparser
+import argparse
 
 config = configparser.ConfigParser()
 config.read('load.ini')
 
+parser = argparse.ArgumentParser(description="Clone repos from Github")
+parser.add_argument("-u", dest="username", required=True, type=str)
+parser.add_argument("-p", dest="path", required=True, type=str)
+args = parser.parse_args()
+
 
 def get_name_of_repo() -> list:
     resp = []
-    username = config.get("MAIN").get("username")
-    soup = BeautifulSoup(requests.get(f'https://github.com/{username}?tab=repositories').text, 'lxml')
+    username = config['MAIN']['username']
+    soup = BeautifulSoup(requests.get(f'https://github.com/{args.username}?tab=repositories').text, 'lxml')
     div = soup.find('div', id='user-repositories-list')
     name = div.find_all('a')
     for n in name:
@@ -20,7 +26,9 @@ def get_name_of_repo() -> list:
 
 def main():
     list_of_repo = get_name_of_repo()
+    os.chdir(args.path)
     for repo in list_of_repo:
+        print(f'Cloning https://github.com{repo}')
         os.system(f'git clone https://github.com{repo}')
 
 
