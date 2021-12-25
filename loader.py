5e7f5d52acd1974371c30a3485fa4fd69638db56
vallenov <a2lenov@mail.ru>
2021-12-25 23:01:23 +0300

    Удалено взаимодействие с ini файлом

--- loader.py
+++ loader.py
@@ -1,11 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import os
-import configparser
 import argparse
-
-config = configparser.ConfigParser()
-config.read('load.ini')
 
 parser = argparse.ArgumentParser(description="Clone repos from Github")
 parser.add_argument("-u", dest="username", required=True, type=str)
@@ -15,7 +11,6 @@ args = parser.parse_args()
 
 def get_name_of_repo() -> list:
     resp = []
-    username = config['MAIN']['username']
     soup = BeautifulSoup(requests.get(f'https://github.com/{args.username}?tab=repositories').text, 'lxml')
     div = soup.find('div', id='user-repositories-list')
     name = div.find_all('a')
